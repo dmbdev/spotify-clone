8a254dca4fb1633b47e9f357da92bad02a72ce38
dmbdev <dxniel.martin@gmail.com>
2025-06-11 09:47:56 +0200

    added principal functionality

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    const currentTime = new Date()
+    const currentHour = currentTime.getHours()
+
+    let greetig = ""
+    
+    if (currentHour < 12) {
+        greetig = "Buenos días"
+    } else if (currentHour < 18) {
+        greetig = "Buenas tardes"
+    } else {
+        greetig = "Buenas noches"
+    }
+</script>
+
+<h1 class=" text-3xl font-bold">{greetig}</h1>
