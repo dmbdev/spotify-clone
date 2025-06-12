5f7cfbcbc01883a00dd6175a333d921621efd8da
dmbdev <dxniel.martin@gmail.com>
2025-06-12 09:44:54 +0200

    added next/prev song functionality

--- src/components/CardPlayButton.tsx
+++ src/components/CardPlayButton.tsx
@@ -30,7 +30,7 @@ export function CardPlayButton ({ id, size = 'small' }) {
 
 
   return (
-    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400">
+    <button onClick={handleClick} className="card-play-button rounded-full bg-green-500 p-4 hover:scale-105 transition hover:bg-green-400 text-black">
       {isPlayingPlaylist ? <Pause className={iconClassName} /> : <Play className={iconClassName} />}
     </button>
   )
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,14 +4,17 @@ import { usePlayerStore } from "src/store/playerStore"
 import { PlayerCurrentSong } from "./PlayerCurrentSong"
 import { PlayerSoundControl } from "./PlayerSoundControl"
 import { PlayerVolumeControl } from "./PlayerVolumeControl"
-import { Pause, Play } from "@icons/PlayerIcons"
+import { Pause, Play, Prev, Next } from "@icons/PlayerIcons"
  
 export function Player () {
-    const { currentMusic, isPlaying, setIsPlaying, volume } = usePlayerStore(state => state)
+    const { currentMusic, isPlaying, setIsPlaying, volume, setCurrentMusic } = usePlayerStore(state => state)
     const audioRef = useRef()
 
     useEffect(() => {
-        isPlaying ? audioRef.current.play() : audioRef.current.pause()
+        if (!currentMusic.song) {
+            return;
+        }
+        isPlaying ? audioRef.current.play().catch((e) => console.log('error playing: ', e)) : audioRef.current.pause()
     }, [isPlaying])
 
     useEffect(() => {
@@ -34,19 +37,57 @@ export function Player () {
         }
     }
 
+    const getCurrentSongIndex = () => {
+    if (currentMusic.songs.length === 0) return -1;
+    return currentMusic.songs.findIndex(e => e.id === currentMusic.song.id) ?? -1
+    }
+
+    const onNextSong = () => {
+        const {songs} = currentMusic;
+        const playlistSongs = songs.length;
+        if (playlistSongs === 0) return;
+
+        const index = getCurrentSongIndex()
+        if (index + 1 < playlistSongs) {
+        updateCurrentMusicBySongId(index + 1)
+        }
+    }
+
+    const onPrevSong = () => {
+        const index = getCurrentSongIndex()
+        if (index > 0) {
+        updateCurrentMusicBySongId(index - 1)
+        }
+    }
+
+    const updateCurrentMusicBySongId = (songIndex) => {
+        const {playlist, songs} = currentMusic;
+        setIsPlaying(false);
+        setCurrentMusic({songs, playlist, song: songs[songIndex]})
+        setIsPlaying(true);
+    }
+
     return (
-        <div className="flex flex-row justify-between w-full px-4 z-50">
+        <div className="flex flex-row justify-between w-full px-2 z-50">
             <div className="w-[200px]">
                 <PlayerCurrentSong {... currentMusic.song} />
             </div>
 
             <div className="grid place-content-center gap-4 fle-1">
                 <div className="flex justify-center flex-col items-center">
-                    <button className="bg-white rounded-full p-2" onClick={handleClick}>
-                        {isPlaying ? <Pause /> : <Play />}
+                    <div className="flex justify-center flex-row flex-nowrap items-center gap-4">
+                    <button className="hover:scale-110 transition duration-300" title="Previous song" onClick={onPrevSong}>
+                        <Prev/>
                     </button>
+                    <button className="bg-white text-black rounded-full p-2 hover:scale-110 transition duration-300" onClick={handleClick}>
+                            {isPlaying ? <Pause/> : <Play/>}
+                    </button>
+                    <button className="hover:scale-110 transition duration-300" title="Next song" onClick={onNextSong}>
+                        <Next/>
+                    </button>
+                </div>
                     <PlayerSoundControl audio={audioRef} />
-                    <audio ref={audioRef} />  
+                    <audio ref={audioRef} onEnded={onNextSong} />  
                 </div>
             </div>
 
